baa78328f4fb0674b567db41baaa67befc9178c6
aabdul015 <160515300+aabdul015@users.noreply.github.com>
2024-04-21 12:24:13 -0500

    Add files via upload

---
+++ PTO.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from tkinter import messagebox as mb
+
+
+main = Tk()
+main.title('Welcome to PTO')
+main.geometry('200x200')
+main.configure(bg='green')
+
+name = input("Greetings!, Please enter your name: ")
+
+def PTO():
+    response = mb.askyesno("Are you getting PTO?", "Are you sure, " + name)
+    Label(main,text=response).pack()
+    if response == 1:
+        mb.showinfo('1', 'Your request has been approved!, ' + name)
+    else:
+        mb.showinfo('1', 'Your request has been denied!, ' + name )
+
+
+respond = Button(main, text="Are you getting PTO?", command=PTO, bg='green', fg='white').pack()
+
+
+main.mainloop()
