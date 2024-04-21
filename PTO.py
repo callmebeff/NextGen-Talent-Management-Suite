c1412dfd1f53d46cb7325ddcbc059a8573d238af
aabdul015 <160515300+aabdul015@users.noreply.github.com>
2024-04-21 13:04:54 -0500

    Add files via upload

--- PTO.py
+++ PTO.py
@@ -5,11 +5,12 @@ from tkinter import messagebox as mb
 main = Tk()
 main.title('Welcome to PTO')
 main.geometry('480x480')
+main.configure(bg='black')
 
-
-name = input("Greetings!, Please enter your name: ")
+name = input("Greetings!, please enter your name: ")
 
 def PTO():
+
     response = mb.askyesno("Are you getting PTO?", "Are you sure, " + name)
     Label(main,text=response).pack()
     if response == 1:
