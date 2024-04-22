6db523f035a0f29d5c1ba94224de7c499cd0d515
aabdul015 <160515300+aabdul015@users.noreply.github.com>
2024-04-22 11:43:12 -0500

    Add files via upload

--- PTO.py
+++ PTO.py
@@ -1,25 +1,33 @@
 from tkinter import *
 from tkinter import messagebox as mb
 
-
 main = Tk()
-main.title('Welcome to PTO')
+main.title('Welcome to PTO!')
 main.geometry('480x480')
-main.configure(bg='black')
+main.configure(bg='light gray')
 
-name = input("Greetings!, please enter your name: ")
+name = Entry(main)
+name.pack()
+
+
+def input():
+    greet = "Greetings, " + name.get()
+    Label(main, text=greet)
+    mb.showinfo('1', greet)
+
+inputButton = Button(main, text="Enter your name", command=input, bg ='green', fg='white')
+inputButton.pack()
 
 def PTO():
-
-    response = mb.askyesno("Are you getting PTO?", "Are you sure, " + name)
+    response = mb.askyesno("Are you getting PTO?", "Are you sure, " + name.get())
     Label(main,text=response).pack()
     if response == 1:
-        mb.showinfo('1', 'Your request has been approved!, ' + name)
+        mb.showinfo('1', 'Your request has been approved!, ' + name.get())
     else:
-        mb.showinfo('1', 'Your request has been denied!, ' + name )
+        mb.showinfo('1', 'Your request has been denied!, ' + name.get())
 
 
-respond = Button(main, text="Are you getting PTO?", command=PTO, bg='green', fg='white').pack()
-
+responseButton = Button(main, text="Are you getting overtime?", command=PTO, bg ='blue', fg='white')
+responseButton.pack()
 
 main.mainloop()
