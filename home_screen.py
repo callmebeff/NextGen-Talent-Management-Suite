a6a2f77894b9670502a4646a790e243f3013f022
Jeff <callmebeff@gmail.com>
2024-04-22 12:07:42 -0500

    updated home_screen

--- home_screen.py
+++ home_screen.py
@@ -2,11 +2,6 @@ import customtkinter as ctk
 import emp_management_screen
 from emp_management_screen import EmployeeManagementScreen
 
-
-
-
-
-
 class App(ctk.CTk):
 
     def __init__(self):
@@ -27,39 +22,33 @@ class App(ctk.CTk):
         subheading.grid(column=0, row=1, padx=20, pady=5, sticky="ew")
 
         # Button: Add/remove employee
-        add_rem_button = ctk.CTkButton(self, text='Add/Remove Employee', font=('CTkFont', 15), height=40, command=self.open_employee_management_screen)
+        add_rem_button = ctk.CTkButton(self, text='Add, Remove, or Search for Employee', font=('CTkFont', 15), height=40, command=self.open_employee_management_screen)
         add_rem_button.grid(column=0, row=2, padx=20, pady=15, sticky="ew")
-
-        # Button: Lookup Employee
-        emp_lookup_button = ctk.CTkButton(self, text='Employee Lookup', font=('CTkFont', 15), height=40)
-        emp_lookup_button.grid(column=0, row=3, padx=20, pady=15, sticky="ew")
 
         # Button: Update user clearance level
         clearance_button = ctk.CTkButton(self, text='Update Clearance', font=('CTkFont', 15), height=40)
-        clearance_button.grid(column=0, row=4, padx=20, pady=15, sticky="ew")
+        clearance_button.grid(column=0, row=3, padx=20, pady=15, sticky="ew")
 
         # Button: Overtime: Approve/Reject
         overtime_button = ctk.CTkButton(self, text='Manage Overtime Requests', font=('CTkFont', 15), height=40)
-        overtime_button.grid(column=0, row=5, padx=20, pady=15, sticky="ew")
+        overtime_button.grid(column=0, row=4, padx=20, pady=15, sticky="ew")
 
         # Button: PTO: Approve/Reject
         pto_button = ctk.CTkButton(self, text='Manage PTO Requests', font=('CTkFont', 15), height=40)
-        pto_button.grid(column=0, row=6, padx=20, pady=15, sticky="ew")
+        pto_button.grid(column=0, row=5, padx=20, pady=15, sticky="ew")
 
         # Button: Add/Remove Employee Certification
         cert_button = ctk.CTkButton(self, text='Add/Remove Employee Certification', font=('CTkFont', 15), height=40)
-        cert_button.grid(column=0, row=7, padx=20, pady=15, sticky="ew")
+        cert_button.grid(column=0, row=6, padx=20, pady=15, sticky="ew")
 
         # Button: Update Employee Pay Rate
         add_rem_button = ctk.CTkButton(self, text='Update Employee Pay Rate', font=('CTkFont', 15), height=40)
-        add_rem_button.grid(column=0, row=8, padx=20, pady=15, sticky="ew")
+        add_rem_button.grid(column=0, row=7, padx=20, pady=15, sticky="ew")
 
     def open_employee_management_screen(self):
-        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
-            emp_management_screen.main()  # create window if its None or destroyed
-        else:
-            self.toplevel_window.focus()  # if window exists focus it
-
+        emp_management_screen.main()  # create window if its None or destroyed
+    
+    
 
 
 app = App()
